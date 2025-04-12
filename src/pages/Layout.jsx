40bacfc69e1d9383b99859a8172f3fd585c54f88
Александр Варламов <aleksandrvarlamov@Aleksandrs-MacBook-Pro.local>
2025-04-12 15:43:54 +0300

    Added Footer component  --demo

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,6 @@
 import HeaderComponent from "../components/HeaderComponent.jsx";
 import {Outlet} from "react-router";
+import FooterComponent from "../components/FooterComponent.jsx";
 
 export default function Layout() {
 
@@ -7,6 +8,7 @@ export default function Layout() {
         <div className="bg-white">
             <HeaderComponent/>
             <Outlet/>
+            <FooterComponent/>
         </div>
     )
 }
