e816def880dc57a458b41d9a2b028ad4325004c4
IndieVar <aleksvarlaam@gmail.com>
2025-04-30 17:16:11 +0300

    Fixed .png 403 forbidden

--- Dockerfile
+++ Dockerfile
@@ -40,6 +40,8 @@ FROM nginx
 # Copy built application
 COPY --from=build /app/dist /usr/share/nginx/html
 
+RUN chmod 644 ./usr/share/nginx/html/*.png
+
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 80
 CMD [ "/usr/sbin/nginx", "-g", "daemon off;" ]
--- app/router.jsx
+++ app/router.jsx
@@ -1,12 +1,13 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router";
 import HomePage from "../src/pages/HomePage.jsx";
 import Layout from "../src/pages/Layout.jsx";
+import NotFoundPage from "../src/pages/NotFoundPage.jsx";
 
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-            <Route element={<Layout/>}>
+            <Route element={<Layout/>} errorElement={<NotFoundPage/>}>
                 <Route path="/" element={<HomePage/>}/>
             </Route>
         </>
